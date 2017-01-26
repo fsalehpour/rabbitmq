e1aea5f68c02fec831e1931cdfe1d92100d51639
Faramarz Salehpour <subs@marz.co>
2017-01-26 10:35:10 +0330

    Rename methods and style the code according to PSR-2

--- src/RabbitMQ.php
+++ src/RabbitMQ.php
@@ -5,35 +5,63 @@ use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPSocketConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+/**
+ * Class RabbitMQ
+ * @package RabbitMQWrapper
+ */
 class RabbitMQ
 {
+    /**
+     * @var AMQPSocketConnection
+     */
     private $connection;
 
     /**
      * RabbitMQ constructor.
+     * @param string $host
+     * @param int    $port
+     * @param string $user
+     * @param string $password
+     * @param string $vhost
      */
     public function __construct($host = 'localhost', $port = 5672, $user = 'guest', $password = 'guest', $vhost = '/')
     {
         $this->connection = new AMQPSocketConnection($host, $port, $user, $password, $vhost);
     }
 
+    /**
+     * @return AMQPChannel
+     */
     public function channel()
     {
         return $this->connection->channel();
     }
 
+    /**
+     * @return \PhpAmqpLib\Wire\IO\SocketIO
+     */
     public function getSocket()
     {
         return $this->connection->getSocket();
     }
 
     /**
-     * @param $ch
-     * @param $msg
+     * @param AMQPMessage $msg
+     * @param             $exchange
+     * @param             $routing_key
+     * @param null        $timeout
+     * @param AMQPChannel $channel
      * @return mixed
+     * @internal param $ch
      */
-    public function confirmed_publish(AMQPMessage $msg, $exchange, $routing_key, $timeout=null, AMQPChannel $channel = null)
-    {
+    public function confirmedPublish(
+        AMQPMessage $msg,
+        $exchange,
+        $routing_key,
+        $timeout = null,
+        AMQPChannel $channel = null
+    ) {
+    
         $ch = $channel ?: $this->channel();
         $response = true;
         $ch->set_ack_handler(function (AMQPMessage $msg) use (&$response) {
@@ -42,9 +70,11 @@ class RabbitMQ
         $ch->set_nack_handler(function (AMQPMessage $msg) use (&$response) {
             $response = $response && false;
         });
-        $ch->set_return_listener(function ($replyCode, $replyText, $exchange, $routingKey, AMQPMessage $msg) use (&$response) {
-            $response = $response && false;
-        });
+        $ch->set_return_listener(
+            function ($replyCode, $replyText, $exchange, $routingKey, AMQPMessage $msg) use (&$response) {
+                $response = $response && false;
+            }
+        );
 
         $ch->confirm_select();
         $ch->basic_publish($msg, $exchange, $routing_key, true);
